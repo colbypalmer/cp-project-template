d24d9cb2037f1a57e07962646cc9a512ac48017b
Colby Palmer <colby@colbypalmer.com>
2014-10-21 21:55:01 -0700

    fix settings links and remove duplicate admin from INSTALLED_APPS

--- project/project/settings.py
+++ project/project/settings.py
@@ -106,7 +106,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.comments',
     'project',
