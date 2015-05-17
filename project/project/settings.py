e54caed07b56cd373956d13e449a32472d05e3e6
Colby Palmer <colby@colbypalmer.com>
2015-05-17 00:42:19 -0700

    Upgrade to Django 1.8.1

--- project/project/settings.py
+++ project/project/settings.py
@@ -17,35 +17,16 @@ ALLOWED_HOSTS = []
 
 MANAGERS = ADMINS
 
-TIME_ZONE = 'America/Los_Angeles'
-
-LANGUAGE_CODE = 'en-us'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-SITE_ID = 1
-SITE_URL = "http://myproject.com"
-SITE_NAME = "My Project"
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
 # URL prefix for static files.
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/admin/static/'
-
-## Additional locations of static files
+# Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -57,41 +38,6 @@ STATICFILES_DIRS = (
 #    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ##    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 #)
-
-# # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    #     'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    'django.core.context_processors.request',
-    "django.contrib.messages.context_processors.messages",
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'project.urls'
-
-WSGI_APPLICATION = 'project.wsgi.application'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, "templates"),
-)
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -108,7 +54,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'project',
-    'south',
 )
 if DEBUG:
     DEBUG_APPS = (
@@ -116,6 +61,49 @@ if DEBUG:
         'debug_toolbar',
     )
     INSTALLED_APPS = INSTALLED_APPS + DEBUG_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
